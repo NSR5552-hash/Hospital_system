3bd798ee4f859e6ba0fa856a30f88e273c507f56
SUM <abushaab5552@gmail.com>
2026-01-19 17:19:11 +0400

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 include("dbconnect.php");
+
+$error = "";
 if (isset($_POST["login"])) {
     $u = $_POST["username"];
     $p = $_POST["password"];
@@ -9,24 +11,38 @@ if (isset($_POST["login"])) {
     if (mysqli_num_rows($q) == 1) {
         $_SESSION["user"] = $u;
         header("Location: index.php");
+        exit();
+    } else {
+        $error = "Invalid username or password!";
     }
 }
 ?>
 <!DOCTYPE html>
 <html>
 <head>
+    <title>MFCC - Login</title>
     <link rel="stylesheet" href="style.css">
-    <title>Login</title>
 </head>
 <body>
-    <div class="container">
+    <!-- Navbar -->
+    <div class="navbar">
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+    </div>
+
+    <!-- Login Box -->
+    <div class="login-box">
         <h2>Login</h2>
+        <?php if($error): ?>
+            <p style="color: red;"><?php echo $error; ?></p>
+        <?php endif; ?>
         <form method="POST">
-            <input name="username" placeholder="Username">
-            <input type="password" name="password" placeholder="Password">
-            <button name="login">Login</button>
+            <input type="text" name="username" class="input-box" placeholder="Username" required>
+            <input type="password" name="password" class="input-box" placeholder="Password" required>
+            <button type="submit" name="login" class="btn">Login</button>
         </form>
-        <p>admin / admin</p>
+        <p class="note">Default: admin / admin123</p>
     </div>
 </body>
 </html>
+
